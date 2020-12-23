347d7545967271ca6492e88f06f83d4a638e76f3
Steve B <sbogucki@mail.usf.edu>
2020-12-22 20:09:54 -0800

    fixed bug run count on landing page

--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -3,16 +3,14 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Landing(props) {
-	const [loaded, setLoaded] = useState(false);
 	const [count, setCount] = useState(640);
 	useEffect(()  => {		
 		setCount(props.data.runs.length);
-		setLoaded(true);
 	}, [props.data.runs.length])	
 
 	let view = <div><h4>Loading...</h4></div>
 
-	if(loaded){
+	if(count > 0){
 		view = 
 			<div>
 				<div>
