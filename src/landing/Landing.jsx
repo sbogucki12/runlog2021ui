448f85d74120f56f788247981d97888ab635d714
Steve B <sbogucki@mail.usf.edu>
2020-12-22 19:00:55 -0800

    updated readme

--- src/landing/Landing.jsx
+++ src/landing/Landing.jsx
@@ -1,22 +1,36 @@
 import './landing.css';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Landing(props) {
-	const count = props.data.runs.length;
+	const [loaded, setLoaded] = useState(false);
+	const [count, setCount] = useState(640);
+	useEffect(()  => {		
+		setCount(props.data.runs.length);
+		setLoaded(true);
+	}, [props.data.runs.length])	
 
+	let view = <div><h4>Loading...</h4></div>
+
+	if(loaded){
+		view = 
+			<div>
+				<div>
+						<span role="img" aria-label="runner" id="runnerIcon">
+							🏃
+						</span>
+					</div>
+					<p>Currently {count} consecutive daily runs</p>
+					<p>And counting!</p>
+					<Link to="/runlog">
+						<button>Enter</button>{' '}
+					</Link>
+			</div>
+	}
 	return (
 		<div id="landingContainer">
 			<div id="landingBox">
-				<div>
-					<span role="img" aria-label="runner" id="runnerIcon">
-						🏃
-					</span>
-				</div>
-				<p>Currently {count} consecutive daily runs</p>
-				<p>And counting!</p>
-				<Link to="/runlog">
-					<button>Enter</button>{' '}
-				</Link>
+				{view}
 			</div>
 		</div>
 	);
